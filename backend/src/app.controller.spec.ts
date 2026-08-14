f739151892b9cf2d89fbcfe8e068119718959a07
Quinn Agas <quinnagas@gmail.com>
2026-08-14 20:56:09 +0800

    Set up NestJS Prisma and PostgreSQL

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,22 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: PrismaService,
+          useValue: {
+            $queryRaw: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = module.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('health', () => {
+    it('should return API health status', async () => {
+      await expect(appController.health()).resolves.toEqual({
+        status: 'ok',
+        database: 'connected',
+      });
     });
   });
 });
